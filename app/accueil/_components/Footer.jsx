4eeb51a255d23fda6601a72244df449b84d442fa
Lionel  Eda <lioneleda.dev@gmail.com>
2024-09-10 05:15:09 +0100

    modification de la taille du logo footer

--- app/accueil/_components/Footer.jsx
+++ app/accueil/_components/Footer.jsx
@@ -7,7 +7,7 @@ function Footer() {
     <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 lg:px-8">
       <div className="sm:flex sm:items-center sm:justify-between">
         <div className="flex justify-center text-teal-600 sm:justify-start">
-          <Image src='/Chancimi.svg' width={100} height={100} alt='Chancimi' />
+          <Image src='/Chancimi.svg' width={64} height={64} alt='Chancimi' />
         </div>
   
         <p className="mt-4 text-center text-sm text-gray-500 lg:mt-0 lg:text-right">
