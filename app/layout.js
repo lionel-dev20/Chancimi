833bcf8d6a1b3fe11c19d560fc1f8a4c65face86
Lionel  Eda <lioneleda.dev@gmail.com>
2024-09-12 02:54:16 +0100

    correction de l'erreur pour le provider clerk en locla

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,7 @@
-
 import { Inter, Outfit } from "next/font/google";
 import "./globals.css";
-import {   ClerkProvider } from "@clerk/nextjs";
-import { Toaster } from "@/components/ui/sonner"
-
+import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "@/components/ui/sonner";
 
 const inter = Outfit({ subsets: ["latin"] });
 
@@ -12,15 +10,15 @@ export const metadata = {
   description: "Passer le permi de conduire en 2 3 mois et plus en tout tranquilité grace à notre école de formation",
 };
 
-export default function RootLayout({ children,  pageProps:{...pageProps}}) {
+export default function RootLayout({ children }) {
   return (
-    <ClerkProvider {...pageProps} publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
-    <html lang="en">
-      <body className={inter.className}>
-        {children}
-        <Toaster />
+    <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
+      <html lang="en">
+        <body className={inter.className}>
+          {children}
+          <Toaster />
         </body>
-    </html>
+      </html>
     </ClerkProvider>
   );
 }
