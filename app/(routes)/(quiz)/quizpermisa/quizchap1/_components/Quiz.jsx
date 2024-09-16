99884637c40d7667e6dcdb009a38b963e72bfe72
Lionel  Eda <lioneleda.dev@gmail.com>
2024-09-16 09:38:42 +0100

    mise à jour des quiz

--- app/(routes)/(quiz)/quizpermisa/quizchap1/_components/Quiz.jsx
+++ app/(routes)/(quiz)/quizpermisa/quizchap1/_components/Quiz.jsx
@@ -58,13 +58,13 @@ export default function Quiz() {
   };
 
   return (
-    <div className='grid grid-cols-1 md:grid-cols-4 p-10 gap-5 bg-white m-10 rounded-[8px]'>
+    <div className='grid grid-cols-1 p-10 gap-5 bg-white m-10 rounded-[8px]'>
       {timeLeft > 0 && !quizFinished && (
-        <div className=''>
-          <h2 className='p-4  text-2xl font-bold bg-primary w-auto block rounded-[4px] text-white'>Il vous reste : {timeLeft}</h2>
+        <div className='w-full'>
+          <h2 className='p-4  text-2xl font-bold bg-primary max-w-[18rem] block rounded-[4px] text-white'>Il vous reste : {timeLeft}</h2>
           <h3 className='border-b-2 border-gray-100 text-xl font-bold text-gray-700 my-6'>Question {currentQuestion + 1} sur {dataquizchap1.length}</h3>
           <p className='text-[20px] font-semibold text-gray-700 py-4' >{dataquizchap1[currentQuestion].question}</p>
-          <div>
+          <div className='w-full'> 
             {dataquizchap1[currentQuestion].options.map((option, index) => (
               <button key={index} onClick={() => handleAnswer(option)} className='flex flex-col-1 gap-4 border border-gray-50 py-4 px-10 rounded-[4px]'>
                 {option}
--- app/(routes)/(quiz)/quizpermisa/quizchap1/dataquizchap1.js
+++ app/(routes)/(quiz)/quizpermisa/quizchap1/dataquizchap1.js
@@ -1,15 +1,81 @@
 // quiz du chaptire 1 du cours Permis A
 const dataquizchap1 = [
     {
-      question: "Quelle est la capitale de la France?",
-      options: ["Paris", "Londres", "Berlin", "Madrid"],
+      question: "1- La signalisation routière permet de rendre la route muette",
+      options: ["a) vrai", "b) Faux"],
       correctAnswer: "Paris",
     },
     {
-      question: "Quel est le plus grand océan du monde?",
-      options: ["Pacifique", "Atlantique", "Indien", "Arctique"],
+      question: "2-Selectionnez la réponse juste: deux signalisation à l’entrée d’une agglomération",
+      options: ["a) Le panneau de limitation de vitesse et les panneaux de direction", 
+        "b) Les panneaux de prescription absolue et les panneaux d’indication", 
+        "c)les panneaux de danger et les panneaux d’obligation"],
       correctAnswer: "Pacifique",
     },
+    {
+      question: "3-Deux signalisation à la sortie d’une agglomération",
+      options: ["a) Le panneau de limitation de vitesse de le panneau de direction ", 
+        "b) Le panneau de danger et le panneau d’obligation", 
+        "c) Le panneau de sortie d’agglomération et les panneaux de direction "],
+      correctAnswer: "Pacifique",
+    },
+    {
+      question: "2-Selectionnez la réponse juste: deux signalisation à l’entrée d’une agglomération",
+      options: ["a) Le panneau de limitation de vitesse et les panneaux de direction", 
+        "b) Les panneaux de prescription absolue et les panneaux d’indication", 
+        "c) La ligne discontinue jaune et le zigzag de couleur jaune"],
+      correctAnswer: "Pacifique",
+    },
+    {
+      question: "5-Deux signalisation qui interdisent le stationnement",
+      options: ["a) La ligne discontinue rouge blanc et la zone zébra", 
+        "b) La ligne de rive continue jaune et la ligne discontinue jaune", 
+        "c) La ligne discontinue jaune et le zigzag de couleur jaune"],
+      correctAnswer: "Pacifique",
+    },
+    {
+      question: "6- Que signifie : la ligne de rive jaune continue",
+      options: ["a) Stationnement interdit", 
+        "b) Arrêt interdit", 
+        "c) Arrêt et stationnement interdit"],
+      correctAnswer: "Pacifique",
+    },
+    {
+      question: "7- La ligne de rive jaune discontinue",
+      options: ["a) Stationnement interdit", 
+        "b) Arrêt interdit", 
+        "c) Arrêt et stationnement interdit "],
+      correctAnswer: "Pacifique",
+    },
+    {
+      question: "2-Selectionnez la réponse juste: deux signalisation à l’entrée d’une agglomération",
+      options: ["a) Le panneau de limitation de vitesse et les panneaux de direction", 
+        "b) Les panneaux de prescription absolue et les panneaux d’indication", 
+        "c)les panneaux de danger et les panneaux d’obligation"],
+      correctAnswer: "Pacifique",
+    },
+    {
+      question: "8-La ligne de rive alternative rouge blanc",
+      options: ["a) Stationnement interdit", 
+        "b) Arrêt interdit", 
+        "c) Arrêt et stationnement interdit"],
+      correctAnswer: "Pacifique",
+    },
+    {
+      question: "9- Le panneau de danger est de forme",
+      options: ["a) Rectangulaire", 
+        "b) Triangulaire", 
+        "c) Aucune réponse juste"],
+      correctAnswer: "Pacifique",
+    },
+    {
+      question: "10- La signalisation routière comprend",
+      options: ["a) Deux grands ensembles", 
+        "b) Trois grands ensembles", 
+        "c) Une multitude d’ensembles"],
+      correctAnswer: "Pacifique",
+    },
+    
     // Ajoutez plus de questions ici
   ];
   
