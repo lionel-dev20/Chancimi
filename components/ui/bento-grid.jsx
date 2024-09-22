a56b04b5e4e8a5df8f4c9d5021833466357453a9
Lionel  Eda <lioneleda.dev@gmail.com>
2024-09-22 17:46:24 +0100

    reactivation des icon bento-grid

--- components/ui/bento-grid.jsx
+++ components/ui/bento-grid.jsx
@@ -35,7 +35,7 @@ export const BentoGridItem = ({
         {image}
       </div>
       <div className="group-hover/bento:translate-x-2 transition duration-200">
-        {/* {icon} */}
+        {icon} 
         <div
           className=" font-bold text-neutral-600 dark:text-neutral-200 mb-2 mt-2">
           {title}
