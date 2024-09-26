a252abb06800f3a16ad8732258bcac74a0a6e755
Lionel  Eda <lioneleda.dev@gmail.com>
2024-09-26 19:20:44 +0100

    correction du fichier route.js de bookLesson

--- app/api/bookLesson/route.js
+++ app/api/bookLesson/route.js
@@ -3,6 +3,7 @@ import { google } from 'googleapis';
 const SCOPES = ['https://www.googleapis.com/auth/calendar.events'];
 const calendar = google.calendar('v3');
 
+// Fonction pour créer un événement sur Google Calendar
 const createEvent = async (auth, eventDetails) => {
   try {
     const response = await calendar.events.insert({
@@ -17,6 +18,10 @@ const createEvent = async (auth, eventDetails) => {
   }
 };
 
+// Utilisation du nouveau système de configuration
+export const dynamic = 'force-dynamic';
+
+// API handler pour la création d'événements
 export default async function handler(req, res) {
   if (req.method === 'POST') {
     const { fullName, phoneNumber, neighborhood, date, time } = req.body;
@@ -26,25 +31,25 @@ export default async function handler(req, res) {
       description: `Rendez-vous pour un cours pratique avec ${fullName}, Téléphone: ${phoneNumber}, Quartier: ${neighborhood}`,
       start: {
         dateTime: `${date}T${time}:00`,
-        timeZone: 'Europe/Paris',
+        timeZone: 'Europe/Paris', // Ajustez la timezone si nécessaire
       },
       end: {
         dateTime: `${date}T${parseInt(time.split(':')[0]) + 1}:${time.split(':')[1]}:00`,
         timeZone: 'Europe/Paris',
       },
-      attendees: [{ email: 'agence.chancimmi@gmail.com' }],
+      attendees: [{ email: 'agence.chancimmi@gmail.com' }], // Remplacez par votre email
     };
 
     try {
       const auth = new google.auth.GoogleAuth({
-        keyFile: 'googleCalendar.json', // Assurez-vous que ce chemin est correct
+        keyFile: 'path-to-your/googleCalendar.json', // Chemin vers la clé de votre compte de service
         scopes: SCOPES,
       });
       const authClient = await auth.getClient();
       const event = await createEvent(authClient, eventDetails);
       res.status(200).json({ success: true, event });
     } catch (error) {
-      console.error('Error with Google Calendar API:', error); 
+      console.error('Erreur avec Google Calendar API:', error);
       res.status(500).json({ success: false, message: 'Erreur avec Google Calendar', error: error.message });
     }
   } else {
