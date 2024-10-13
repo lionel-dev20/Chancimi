fbeb7f69ed92a3217bea12623596a0eb5f8ab4c9
Lionel  Eda <lioneleda.dev@gmail.com>
2024-10-13 12:21:41 +0100

    mise en ligne

--- app/pratique-course/page.tsx
+++ app/pratique-course/page.tsx
@@ -1,23 +1,26 @@
-
-
 import { Home } from "lucide-react";
 import Link from "next/link";
 
-function page() {
+function Page() {
   return (
     <div className='max-w-7xl flex flex-col items-center justify-center sm:my-6 h-screen mx-auto'>
-        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSd8XXW0I3N1o8IpkM4nYrgml-XHJqC0Q3i6W11m948d-l0rHw/viewform?embedded=true"  width={1024} height={920} frameborder="0" marginheight="0" marginwidth="0">Chargement…</iframe>
+      <iframe 
+        src="https://docs.google.com/forms/d/e/1FAIpQLSd8XXW0I3N1o8IpkM4nYrgml-XHJqC0Q3i6W11m948d-l0rHw/viewform?embedded=true"  
+        width={1024} 
+        height={920} 
+        frameBorder="0"  // Corrigé ici
+        style={{ margin: 0 }}  // Utilisation de `style` pour les marges
+      >
+        Chargement…
+      </iframe>
     
-    
-    
-    <button type="submit" className="sm:w-[40%] sm:text-[1rem] sm:mt-6">
-      <Link href='/' className="flex items-center">
-        Retour à l&apos;accueil <Home className="ml-4"/>
+      <button type="submit" className="sm:w-[40%] sm:text-[1rem] sm:mt-6">
+        <Link href='/' className="flex items-center">
+          Retour à l&apos;accueil <Home className="ml-4"/>
         </Link>
-        </button>
-        
+      </button>
     </div>
-  )
+  );
 }
 
-export default page 
+export default Page;
