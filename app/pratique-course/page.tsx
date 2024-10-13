582142c598f55b6e3903b6e08cb0804687f9352c
Lionel  Eda <lioneleda.dev@gmail.com>
2024-10-13 12:14:30 +0100

    fais ceci

--- app/pratique-course/page.tsx
+++ app/pratique-course/page.tsx
@@ -1,5 +1,6 @@
-import { Home } from "lucide-react"
-import { Button } from '/components/ui/button';
+
+
+import { Home } from "lucide-react";
 import Link from "next/link";
 
 function page() {
@@ -8,11 +9,13 @@ function page() {
         <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSd8XXW0I3N1o8IpkM4nYrgml-XHJqC0Q3i6W11m948d-l0rHw/viewform?embedded=true"  width={1024} height={920} frameborder="0" marginheight="0" marginwidth="0">Chargement…</iframe>
     
     
-        <Button type="submit" className="sm:w-[40%] sm:text-[1rem] sm:mt-6">
+    
+    <button type="submit" className="sm:w-[40%] sm:text-[1rem] sm:mt-6">
       <Link href='/' className="flex items-center">
         Retour à l&apos;accueil <Home className="ml-4"/>
         </Link>
-        </Button>
+        </button>
+        
     </div>
   )
 }
